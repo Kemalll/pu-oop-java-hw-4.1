7939b514a4d280086db23c9abf97b2a2e3605447
Kemalll <63673510+Kemalll@users.noreply.github.com>
2021-02-10 20:13:48 +0200

    Update UncrossableSwamp.java

--- GPS-Bab-Yaga/src/Field/UncrossableSwamp.java
+++ GPS-Bab-Yaga/src/Field/UncrossableSwamp.java
@@ -1,12 +1,30 @@
 package Field;
 
+import GPS.GPS;
+
 import java.awt.*;
+import java.util.Random;
 
 public class UncrossableSwamp extends Field {
-    public UncrossableSwamp(int row, int col, Color color){
-        super(row, col, color);
+    public UncrossableSwamp(Color color) {
+        super(color);
+//        super(row, col, color);
 
-        //for(int swamps=0 ; swamps<=5; swamps ++){}
-
+//        int swamps=0;
+//        do{  Random rand = new Random(GPS.FOREST_SIZE);
+//        row= rand.nextInt(GPS.FOREST_SIZE);
+//        col= rand.nextInt(GPS.FOREST_SIZE);row= rand.nextInt(GPS.FOREST_SIZE);
+//
+//        swamps++;
+//           // System.out.println("ran"+var,"se"+var2);
+//        }
+        //   while (swamps<5);
+        Random rand = new Random(GPS.FOREST_SIZE);
+        for (int swamps = 0; swamps <= 5; swamps++) {
+            row = rand.nextInt(GPS.FOREST_SIZE);
+            col = rand.nextInt(GPS.FOREST_SIZE);
+           // System.out.println("row:"+row+"col:" + col);
+        }
     }
 }
+
